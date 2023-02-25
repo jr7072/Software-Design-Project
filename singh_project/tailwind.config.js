99a306f85db5db96a89b19c4620c89427f9299b8
lvalle-t <lupitavalle713@gmail.com>
2023-02-24 18:39:47 -0600

    Registration_01

--- singh_project/tailwind.config.js
+++ singh_project/tailwind.config.js
@@ -2,9 +2,15 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'registration_pattern': "url('/register_pattern.png')",
+        
+      }
+    },
   },
   plugins: [],
 }
