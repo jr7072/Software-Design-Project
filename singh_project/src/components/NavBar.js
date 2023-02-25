32ea7c886d642816afda02df23ea33d81037f67d
jr7072 <rosales.juan7072@gmail.com>
2023-02-25 01:12:53 -0600

    test

--- singh_project/src/components/NavBar.js
+++ singh_project/src/components/NavBar.js
@@ -3,7 +3,7 @@ const NavBar = () => {
     return (
         <nav class="flex items-center justify-between flex-wrap opacity-1 p-6">
             <div class="flex items-center flex-shrink-0 text-white ml-8">
-                <span class="font-bold text-2xl text-black tracking-tight">Fuel Quotes</span>
+                <span class="font-bold text-2xl text-black tracking-tight">Fuel Quotes Test</span>
             </div>
             <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div class="ml-auto mr-8 hidden">
