35b7536d538fcd5c22560247e1695a73a92d54a8
Juan Rosales <juanrosales@juans-mbp-3.attlocal.net>
2023-03-12 15:12:38 -0500

    stopped package lock json tracking

---
+++ singh_project/src/pages/login.js
@@ -0,0 +1,10 @@
+// login page
+
+const Login = () => {
+
+    return (
+        <h1>Login Page</h1>
+    )
+}
+
+export default Login;
---
+++ singh_project/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'registration_pattern': "url('/register_pattern.png')",
+        'quote_history': "url('/quote_history.png')", 
+        'quoteForm' : "url('/mesh-852.png')",
+        'accountForm' : "url('/mesh-487.png')"
+      },
+      fontFamily: {
+        'inter': ['Inter var', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
