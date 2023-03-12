2a0419c4ca80cbb213f5bb550e58edccc9da4bb1
Juan Rosales <juanrosales@juans-mbp-3.attlocal.net>
2023-03-12 15:09:58 -0500

    generated user data

---
+++ api/controllers/UserController.js
@@ -0,0 +1,19 @@
+// user functions
+
+
+const getUserData  = (id) => {
+    // mock data here will add database call later
+    return -1;
+}
+
+const updateUserData = (id, data) => {
+    return -1;
+}
+
+const createUserData = (data) => {
+    return -1;
+}
+
+const deleteUserData = (id) => {
+    return -1
+}
--- api/routes/UserRouter.js
+++ api/routes/UserRouter.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 // routes here
 
+
 module.exports = router;
