8e91a8d58ec6c4475f716f48ef5050589f40459d
Juan Rosales <juanrosales@juans-mbp-3.attlocal.net>
2023-03-12 19:20:02 -0500

    created client management routes

--- api/models/UserDB.js
+++ api/models/UserDB.js
@@ -3,7 +3,48 @@
 
 
 const getUsers = () => {  
-    return -1;
+    return [
+        {
+            "id": 1,
+            "fullName": "John Doe",
+            "addressLine1": "123 Main St",
+            "addressLine2": null,
+            "city": "Anytown",
+            "zipCode": "12345"
+        },
+        {
+            "id": 2,
+            "fullName": "Jane Smith",
+            "addressLine1": "456 Elm St",
+            "addressLine2": "Apt 2B",
+            "city": "Somecity",
+            "zipCode": "67890"
+        },
+        {
+            "id": 3,
+            "fullName": "Bob Johnson",
+            "addressLine1": "789 Oak St",
+            "addressLine2": null,
+            "city": "Othercity",
+            "zipCode": "24680"
+        },
+        {
+            "id": 4,
+            "fullName": "Sarah Lee",
+            "addressLine1": "432 Park Ave",
+            "addressLine2": "Suite 100",
+            "city": "Bigcity",
+            "zipCode": "54321"
+        },
+        {
+            "id": 5,
+            "fullName": "Mike Smith",
+            "addressLine1": "321 Maple St",
+            "addressLine2": "Unit 5",
+            "city": "Smalltown",
+            "zipCode": "13579"
+        }
+    ];      
 }
 
 
--- api/routes/UserRouter.js
+++ api/routes/UserRouter.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
 
+//internal modules
+const {getUserData, updateUserData} = require('../controllers/UserController');
+const {
+    updateUser,
+    createUser,
+    deleteUser
+} = require('../models/UserDB');
+
 // middleware here
 
+
 // routes here
+router.get('/:id', (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const data = getUserData(id);
+        const json_data = JSON.stringify(data);
+        res.status(200).send(json_data);
+
+    } catch (error) {
+
+        const data = {
+            error: error.message
+        }
+        const json_data = JSON.stringify(data);
+        res.status(400).send(json_data);
+    
+    }
+})
+
+
+router.put('/:id', (req, res) => {
+    try {
+
+        const id = req.params.id;
+        const data = req.body;
+        const updated_data = updateUserData(id, data);
+        // call a database update here
+        const json_data = JSON.stringify(updated_data);
+        res.status(201).send(json_data); 
+
+    } catch (error) {
+
+        const data = {
+            error: error.message
+        }
+        const json_data = JSON.stringify(data);
+        res.status(400).send(error.message);
+        
+    }
+})
 
 
 module.exports = router;
