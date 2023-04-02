6225fd9aad8dd3343e8121cdc5d8a09f44477e7c
samdiobar <sbp5602@gmail.com>
2023-04-02 11:16:28 -0500

    started registration

--- api/test/Registration.test.js
+++ api/test/Registration.test.js
@@ -1,4 +1,4 @@
-const {hashCode, fetchUserAuthData } = require('../controllers/LoginController');
+const {hashCode, fetchUserAuthData } = require('../controllers/RegistrationController');
 const {authDB} = require('../models/UserDB');
 
 jest.mock('../models/UserDB');
