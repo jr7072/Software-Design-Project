5bc84881737f05cfcb9e518035ca915c10c7e33a
Juan Rosales <juanrosales@Juans-MacBook-Pro-3.local>
2023-04-28 17:52:52 -0500

    final

--- api/routes/FuelHistoryRouter.js
+++ api/routes/FuelHistoryRouter.js
@@ -2,45 +2,47 @@
 const express = require('express');
 const router = express.Router();
 const { db } = require('../db/firebase_util.js');
+const { getUserFuelHistoryIds } = require('../controllers/UserController');
 
+router.get('/:id', async (req, res) => {
+  
+  const id = req.params.id;
 
-export default async (req, res) => {
   try {
-    const snapshot = await db.ref('fuelquote').once('value');
-    const data = snapshot.val();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Failed to fetch quote history:', error);
-    res.status(500).json({ message: 'Failed to fetch quote history' });
+
+    const fuel_ids = await getUserFuelHistoryIds(id);
+
+    console.log(fuel_ids)
+    
+    // get the fuel quotes bases on fuel ids
+    const fuel_ref = db.ref('/fuel');
+    const fuel_snapshot = await fuel_ref.once('value');
+    const fuel_data = fuel_snapshot.val();
+
+    const fuelQuotes = [];
+
+
+
+    for (const property in fuel_data) {
+
+      if (fuel_ids.includes(property)) {
+        fuelQuotes.push(fuel_data[property]);
+      }
+
+    }
+
+    json_data = JSON.stringify(fuelQuotes);
+
+    res.status(200).send(json_data);
+
+  } catch (e) {
+    console.log(e);
   }
-};
+
+})
 
 
 module.exports = router;
 
 
-
-
-// const express = require('express');
-// const router = express.Router();
-// const { db } = require('../db/firebase_util.js');
-
-
-
-// let historyRecord = [{
-//     gallonsRequested: 5,
-//     deliveryAddress: "1313 Main st 77093, Houston Texas",
-//     deliveryDate: "03/15/2023",
-//     suggestedPricePerGallon: 5,
-//     totalAmountDue: 25}]
-
-// function getFuelQuoteHistory(req, res) {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.status(200).json(historyRecord);
-// }
-
-// router.get('/', getFuelQuoteHistory)
-
-
-// module.exports = router;
 
