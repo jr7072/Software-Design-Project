735c3d34bf2dd42dc8e9baf4c1dd403b63a1e2c7
samdiobar <sbp5602@gmail.com>
2023-04-28 03:50:33 -0500

    create user when register (also uuid)

--- api/models/UserDB.js
+++ api/models/UserDB.js
@@ -1,3 +1,4 @@
+const { v1 } = require('uuid');
 const { db } = require('../db/firebase_util.js');
 
 
@@ -18,14 +19,36 @@ const getAuth = async (username) => {
 
     //searches through Auth object for user that matches username parameter
     result = -1;
-    for (var i = 1; i < auths.length; i++) {
-        if (auths[i].username == username) {
+    for (const key in auths) {
+        if (auths[key].username == username) {
             //if a match is found, save it in result
-            result = auths[i];
+            result = auths[key];
         }
     }
 
     return result;
+}
+
+const createUser = async (key) => {
+
+    const userRef = db.ref("users");
+    const snapshot = await userRef.once("value");
+    const users = snapshot.val();
+
+    const data = {
+        'firstName': "",
+        'lastName': "",
+        'addressLine1': "",
+        'addressLine2': "",
+        'city': "",
+        'state': "",
+        'zipCode': ""
+    }
+
+    var newUserRef = userRef.child(key);
+    newUserRef.set(data);
+
+    return -1;
 }
 
 const createUserAuth = async (username, hash) => {
@@ -38,19 +61,19 @@ const createUserAuth = async (username, hash) => {
 
     //if username is already taken, return -1
     var authLength = Object.keys(auths).length;
-    for (var i = 1; i < authLength + 1; i++) {
-        if (auths[i].username == username) {
+    for (const key in auths) {
+        if (auths[key].username == username) {
             return -1;
         }
     }
 
     //if username not taken, add to database
     //generate new key based off length of Auth object + 1
-    var key = authLength + 1;
-    key = key.toString();
+    var key = v1();
     //adds new entry with key and result as the value
     var newAuthRef = authRef.child(key);
     newAuthRef.set(result);
+    createUser(key);
 
     return result;
 }
@@ -58,11 +81,6 @@ const createUserAuth = async (username, hash) => {
 const updateUser = (id, data) => {
     userRef = db.ref(`users/${id}`);
     userRef.update(data);
-}
-
-
-const createUser = (data) => {
-    return -1;
 }
 
 const deleteUser = (id) => {
