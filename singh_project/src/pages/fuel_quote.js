816e336d8326625aa45188538f57afdde3691da0
lvalle-t <lupitavalle713@gmail.com>
2023-04-28 13:29:22 -0500

    price

--- singh_project/src/pages/fuel_quote.js
+++ singh_project/src/pages/fuel_quote.js
@@ -12,7 +12,7 @@ const FuelQuote = ( { cookies }) => {
     const [gallons, setGallons] = useState("");
     const [date, setDate] = useState("");
     const [address, setAddress] = useState("");
-    const [price, setPrice] = useState(2.5);
+    const [price, setPrice] = useState(null);
 
     const user_id = cookies.user.slice(1, -1);
 
@@ -40,7 +40,7 @@ const FuelQuote = ( { cookies }) => {
     }
 
     const getFuelPrice = async() => {
-
+        setPrice(null);
         const response = await axios.post(`http://localhost:3080/fuelquote/get_price`, {gallons, address, date});
         const price_data = await response.data.price;
         setPrice(price_data);
@@ -129,7 +129,13 @@ const FuelQuote = ( { cookies }) => {
                             placeholder="Month/Day/Year"
                         />
 
-                        <div className="mt-1 w-full bg-gray-100 font-semibold text-center text-gray-300 py-2 rounded-md  tracking-wide">{price && <p> Price: {price}</p>}</div>
+                        <div className="mt-1 w-full bg-gray-100 font-semibold text-center text-gray-300 py-2 rounded-md  tracking-wide">
+                            {price === null ? (
+                                <p>Price</p>
+                                    ) : (
+                                <p>Price: {price}</p>
+                            )}
+                        </div>
                     
 
                         <div className='flex justify-between items-center mt-6'>
